2824d2c21d7164861418fc87e176eecb90260756
katakamkumar8 <katakamkumar8@gmail.com>
2025-10-21 00:52:53 +0530

    multi port

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,81 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
+const portfinder = require('portfinder');
 
 // Load environment variables
 dotenv.config({ path: './env.local' });
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+// Set portfinder base port
+portfinder.basePort = parseInt(process.env.PORT) || 3000;
+
+module.exports = async () => {
+  const port = await portfinder.getPortPromise();
+  
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      title: 'Mchat Interface',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env)
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
-    compress: true,
-    port: 3000,
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        title: 'Mchat Interface',
+      }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env)
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: port,
+      host: 'localhost',
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+      },
+      onListening: function (devServer) {
+        if (!devServer) {
+          throw new Error('webpack-dev-server is not defined');
+        }
+
+        const actualPort = devServer.server.address().port;
+        console.log('🚀 Application is running at:');
+        console.log(`   Local:   http://localhost:${actualPort}/`);
+        console.log(`   Network: http://192.168.1.176:${actualPort}/`);
+      },
+      setupExitSignals: true,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
 };
